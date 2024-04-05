916eac514928f0977e962e068ddcc40212c9ea63
anton <ashtanyuk@gmail.com>
2024-04-05 16:56:21 +0300

    YAML добавлен

--- src/main/java/org/example/Prime.java
+++ src/main/java/org/example/Prime.java
@@ -1,2 +1,15 @@
-package org.example;public class Prime {
-}
+package org.example;
+
+public class Prime {
+        public static boolean isPrime(int number) {
+            boolean status = true;
+            for (int j = 2; j * j <= number; j++) {
+                if (number % j == 0) {
+                    status = false;
+                    break;
+                }
+            }
+            return number > 1 ? status : false;
+        }
+    }
+
--- src/test/java/org/example/PrimeTest.java
+++ src/test/java/org/example/PrimeTest.java
@@ -1,4 +1,13 @@
+package org.example;
+
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
+
 public class PrimeTest {
-  
+
+    @org.junit.Test
+    public void isPrime() {
+        Assert.assertEquals(Prime.isPrime(2), true);
+    }
 }
